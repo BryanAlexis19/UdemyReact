055e95ded1639c9f866b96cb5ad2dcedffa13d7c
Bryan Alexis <librale19@gmail.com>
2021-11-23 13:55:49 -0500

    Context managemengt

--- FirstApp/02-EffectsReducersContext/src/store/auth-context.js
+++ FirstApp/02-EffectsReducersContext/src/store/auth-context.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false,
-    onLogout: () => {}
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsloggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    setIsloggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
+    setIsloggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 export default AuthContext;
